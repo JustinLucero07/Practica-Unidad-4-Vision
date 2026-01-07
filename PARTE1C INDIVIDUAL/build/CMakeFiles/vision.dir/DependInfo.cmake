fb2ff946be7dc293511056a3d2dbcb0a3a8b2ed9
JustinLucero07 <justinluceroxd04@gmail.com>
2026-01-06 23:14:52 -0500

    Parte 1c

---
+++ PARTE1C INDIVIDUAL/build/CMakeFiles/vision.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vlarobbyk/Descargas/Ejemplos-de-Código-Visión-Artificial-PG7/Ejemplo-0-Hola-Mundo-C++/Operaciones.cpp" "CMakeFiles/vision.dir/Operaciones.cpp.o" "gcc" "CMakeFiles/vision.dir/Operaciones.cpp.o.d"
+  "/home/vlarobbyk/Descargas/Ejemplos-de-Código-Visión-Artificial-PG7/Ejemplo-0-Hola-Mundo-C++/Principal.cpp" "CMakeFiles/vision.dir/Principal.cpp.o" "gcc" "CMakeFiles/vision.dir/Principal.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ PARTE1C INDIVIDUAL/build/CMakeFiles/vision.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vision.dir/Operaciones.cpp.o"
+  "CMakeFiles/vision.dir/Operaciones.cpp.o.d"
+  "CMakeFiles/vision.dir/Principal.cpp.o"
+  "CMakeFiles/vision.dir/Principal.cpp.o.d"
+  "vision"
+  "vision.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vision.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
